c621c357b22bc5c713464b03db1bb756a14d97da
chrystian <chrystianr@gmail.com>
2019-12-17 17:34:49 +0100

    chore(): adding another demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 
 const longText = `Mega Man X, known in Japan as Rockman X,[a] is an action-platform video game developed and published by Capcom for the Super Nintendo Entertainment System (SNES).
@@ -8,7 +8,7 @@ const longText = `Mega Man X, known in Japan as Rockman X,[a] is an action-platf
 const resetExplanation = `In this text area we want to test if text after some manual changes can be restored to its original value and if textarea keeps autosizing...
 
 
-so just change this text, add some lines... 
+so just change this text, add some lines...
 and in the end hit the 'RESET' button above.
 
 Text area should come back to its original form :)
@@ -19,7 +19,7 @@ Text area should come back to its original form :)
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     public longText = longText;
     public resetableContent = resetExplanation;
     public minRows = 3;
@@ -27,10 +27,16 @@ export class AppComponent {
     public onlyGrow = true;
     public useImportant = true;
 
+    public showAreaInContainer = true;
+
     reactiveText = new FormControl(longText);
     reactiveForm = new FormGroup({
         reactiveText: new FormControl(longText)
     });
+
+    ngOnInit(): void {
+        setTimeout(() => { this.showAreaInContainer = false;});
+    }
 
     changeNgModel() {
         this.longText += "\n next";
